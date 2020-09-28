3cd93b186caef99a5564b9da064d3e315b42f365
86180 <1130600024@qq.com>
2020-09-28 16:57:09 +0800

    双向链表-冒泡排序

--- src/com/linked/list/TwoWayLinkedListDemo.java
+++ src/com/linked/list/TwoWayLinkedListDemo.java
@@ -2,9 +2,11 @@ package com.linked.list;
 
 import com.damin.User;
 
+import java.util.Comparator;
+
 public class TwoWayLinkedListDemo {
     public static void main(String[] args) {
-        TwoWayLinkedList<User> twoWayLinkedList=new TwoWayLinkedList<>(20);
+        TwoWayLinkedList<User> twoWayLinkedList = new TwoWayLinkedList<>(20);
         twoWayLinkedList.add(new User(1, "1号"));
         twoWayLinkedList.add(new User(4, "4号"));
         twoWayLinkedList.add(new User(2, "2号"));
@@ -13,7 +15,10 @@ public class TwoWayLinkedListDemo {
         twoWayLinkedList.add(new User(5, "5号"));
         twoWayLinkedList.remove(new User(2, "2号"));
         twoWayLinkedList.remove(new User(4, "4号"));
-        twoWayLinkedList.insert(2,new User(11, "11号"));
+        twoWayLinkedList.insert(2, new User(11, "11号"));
+        twoWayLinkedList.intercept();
+        twoWayLinkedList.bubbleSort(Comparator.comparingInt(o -> o.age));
+        System.out.println("---------------------------");
         twoWayLinkedList.intercept();
     }
 
@@ -59,7 +64,7 @@ class TwoWayLinkedList<T> {
     public boolean remove(T data) {
         boolean isFind = false;
         NodeT<T> temp = top;
-        while (temp.next != null&&temp.next !=tail) {
+        while (temp.next != null && temp.next != tail) {
             temp = temp.next;
             if (temp.data.equals(data)) {
                 temp.prev.next = temp.next;
@@ -97,11 +102,58 @@ class TwoWayLinkedList<T> {
         current++;
     }
 
-    public void intercept(){
+    public void intercept() {
         NodeT<T> temp = top;
-        while (temp.next!=null){
-            temp=temp.next;
+        while (temp.next != null) {
+            temp = temp.next;
             System.out.println(temp.data.toString());
+        }
+    }
+
+    /**
+     * 冒泡排序
+     *
+     * @param c 比较器
+     */
+    public void bubbleSort(Comparator<? super T> c) {
+        boolean isOrder = false;
+        int end = this.maxSize;
+        while (!isOrder && end > 0) {
+            int start = 0;
+            NodeT<T> temp = top;
+            //是否发生交换
+            boolean replace = false;
+            while (temp.next != null && start < end) {
+                temp = temp.next;
+                start++;
+                //如果下一个元素比当前小，交换位置
+                if (temp.next != null && c.compare(temp.data, temp.next.data) > 0) {
+                    //A B C D --B C 交换位置 B==temp
+                    NodeT<T> nA = temp.prev;
+                    NodeT<T> nc = temp.next;
+                    NodeT<T> nd = temp.next.next;
+                    //A的next指向C
+                    nA.next = temp.next;
+                    //B的prev指向C
+                    temp.prev = nc;
+                    //B的next指向D
+                    temp.next = nd;
+                    //C的上一个指向A
+                    nc.prev = nA;
+                    //C的下一个指向B
+                    nc.next = temp;
+                    //D的上一个指向B
+                    if (nd != null) {
+                        nd.prev = temp;
+                    }
+                    replace = true;
+                }
+            }
+            end--;
+            //没有发生交换则退出
+            if (!replace) {
+                return;
+            }
         }
     }
 }
